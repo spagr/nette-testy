f55744fa8a92c1a135750a80784f74d2b577f545
spagr <spacek@naplne.com>
2015-07-29 17:16:19 +0200

    Config and dd debug function

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -19,3 +19,16 @@ $configurator->addConfig(__DIR__ . '/config/config.local.neon');
 $container = $configurator->createContainer();
 
 return $container;
+
+function dd($var, $title='')
+{
+	$backtrace = debug_backtrace(null/*, 2*/); // Ready for PHP 5.4
+	$source = (isset($backtrace[1]['class'])) ?
+		$backtrace[1]['class'] : 
+		basename($backtrace[0]['file']);
+	$line = $backtrace[0]['line'];
+	if($title !== '')
+		$title .= ' – ';
+	return \Tracy\Debugger::barDump($var, $title . $source . ' (' . $line .')');
+}
+
